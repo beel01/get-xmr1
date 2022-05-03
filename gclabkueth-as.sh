06871e30967bd7d1ebaa862cf1b5842b9a79298c
beel01 <97436451+beel01@users.noreply.github.com>
2022-05-03 16:29:38 +0500

    gclabkueth-as.sh
    kucoin

---
+++ gclabkueth-as.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Install XRDP
+# Before get eth coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R eth
+ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+  tar xf lolMiner_v1.42_Lin64
+  cd 1.42tar.gz 
+  ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user beel01.001
